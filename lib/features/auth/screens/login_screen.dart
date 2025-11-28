daaf20c4af59a24165935339e946d9fcae0b2cba
ShaiGeorge <shaijokuttikkatt@gmail.com>
2025-11-29 00:31:30 +0530

    refactor(auth): Remove unused go_router import from LoginScreen

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
-import 'package:go_router/go_router.dart';
 import '../providers/auth_providers.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
